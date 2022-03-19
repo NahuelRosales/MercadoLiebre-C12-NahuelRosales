0e5640e80e2725aba9c76dc2609ddcc79cf796f4
Nahuel Rosales <nahuelrosales@outlook.com>
2022-03-19 12:31:56 -0300

    working in register view

--- app.js
+++ app.js
@@ -15,4 +15,7 @@ app.listen (PORT, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
 
